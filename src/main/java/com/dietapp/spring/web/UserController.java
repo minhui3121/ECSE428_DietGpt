eb21a3607c42176739ef6bf6a7d005df54d77cfb
artimirchi <156237712+artimirchi@users.noreply.github.com>
2025-10-25 10:07:58 -0400

    update USerController

--- src/main/java/com/dietapp/spring/web/UserController.java
+++ src/main/java/com/dietapp/spring/web/UserController.java
@@ -1,8 +1,12 @@
 package com.dietapp.spring.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dietapp.spring.dto.AuthRequest;
@@ -14,13 +18,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/users/{id}")
     public UserResponseDto findUser(@PathVariable("id") String id){
         return new UserResponseDto(userService.findUserById(id));
     }
 
+    @PostMapping("/users/{id}")
     public UserResponseDto createUser(@RequestBody AuthRequest request) {
         return new UserResponseDto(userService.createUser(request.getName(), request.getEmail(), request.getPassword(), request.getAge(), request.getWeight(), request.isVegetarian(), request.isGlutenFree()));
     }
+
+    @PutMapping("/users/{id}")
     public UserResponseDto updateUser(@PathVariable("id") String id, @RequestBody UserResponseDto dto){
         return new UserResponseDto(userService.updateUser(id, dto));
     }
