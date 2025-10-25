724c9d4947e3320c73063e25c37517efb19702df
artimirchi <156237712+artimirchi@users.noreply.github.com>
2025-10-25 10:42:16 -0400

    created user tests - to be implemented later

---
+++ src/test/java/com/dietapp/repo/UserRepositoryTests.java
@@ -0,0 +1,8 @@
+package com.dietapp.repo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRepositoryTests {
+}
+
---
+++ src/test/java/com/dietapp/service/UserServiceUnitTest.java
@@ -0,0 +1,5 @@
+package com.dietapp.service;
+
+public class UserServiceUnitTest {
+    
+}
